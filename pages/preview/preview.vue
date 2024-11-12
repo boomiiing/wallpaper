88b66fedf30d5d56cff92c469feb972ed25f7b13
HOTLEE_home <1215574240@qq.com>
2024-11-12 23:13:39 +0800

    预览功能

--- pages/preview/preview.vue
+++ pages/preview/preview.vue
@@ -1,30 +1,69 @@
 <template>
-	<view class="preview">
-		<swiper circular>
+	<view class="preview" >
+		<swiper circular @click="maskShow">
 			<swiper-item v-for="item in 5">
 					<image src="../../common/images/preview1.jpg" mode="aspectFill"></image>
 			</swiper-item>
 		</swiper>
+		<view class="mask_box" v-show="showFlag">
+			<view class="goback">
+				<uni-icons  type="left" size="30" ></uni-icons>
+			</view>
+			<view class="count">
+				6/9
+			</view>
+			<view class="detail_time">
+				09:54
+			</view>
+			<view class="date_time">
+				11月12日
+			</view>
+			<view class="foot_box">
+				<view class="info">
+					<view class="item">
+						<view class="">
+							<uni-icons type="info" size="25" ></uni-icons>
+						</view>
+						<view class="">
+							信息
+						</view>
+					</view>
+					<view class="item">
+						<view class="">
+							<uni-icons type="star" size="25" ></uni-icons>
+						</view>
+						<view class="">
+							5分
+						</view>
+					</view>
+					<view class="item">
+						<view class="">
+							<uni-icons type="download" size="25" ></uni-icons>
+						</view>
+						<view class="">
+							下载
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+<script setup>
+	import {ref} from 'vue';
+ let showFlag = ref(true)
+ const maskShow = ()=>{
+	 console.log('asd')
+	 showFlag.value = !showFlag.value
+ }
 </script>
 
 <style lang='scss' scoped>
 	.preview{
 		height: 100vh;
 		width:100%;
+		position: relative;
 		swiper{
 			height: 100%;
 			width:100%;
@@ -34,6 +73,75 @@
 				image{
 					height: 100%;
 					width:100%;
+				}
+			}
+		}
+		.mask_box{
+			.goback{
+				position: absolute;
+				top: 20rpx;
+				left: 20rpx;
+				:deep(){
+					.uni-icons{
+						color:#fff !important
+					}
+				}
+			}
+			.count{
+				text-align: center;
+				position: absolute;
+				top: 120rpx;
+				left: 0;
+				right:0;
+				margin: auto;
+				width:fit-content;
+				/* height:50rpx; */
+				background:rgba(0, 0, 0, .3);
+				padding: 3rpx 5rpx;
+				border-radius: 30rpx;
+				backdrop-filter: blur(10rpx);
+				color:#fff !important
+			}
+			.detail_time{
+				width: 100%;
+				font-size: 130rpx;
+				font-weight: 100;
+				text-align: center;
+				position: absolute;
+				top: 180rpx;
+				left: 0;
+				color:#fff !important
+			}
+			.date_time{
+				width: 100%;
+				text-align: center;
+				position: absolute;
+				top: 350rpx;
+				left: 20rpx;
+				color:#fff !important
+			}
+			.foot_box{
+				width: 100%;
+				display: flex;
+				justify-content:center;
+				align-items: center;
+				position: absolute;
+				bottom: 200rpx;
+				left: 0;
+				.info{
+					width: 60%;
+					height: 100rpx;
+					border-radius: 50rpx;
+					background-color: #fff;
+					display: flex;
+					justify-content: space-around;
+					align-items: center;
+					.item{
+						display: flex;
+						flex-direction: column;
+						align-items: center;
+						font-size: 20rpx;
+					}
 				}
 			}
 		}
--- unpackage/dist/dev/mp-weixin/pages/preview/preview.js
+++ unpackage/dist/dev/mp-weixin/pages/preview/preview.js
@@ -1,19 +1,49 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
 const common_assets = require("../../common/assets.js");
-const _sfc_main = {
-  data() {
-    return {};
-  },
-  methods: {}
-};
-function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {
-    a: common_vendor.f(5, (item, k0, i0) => {
-      return {};
-    }),
-    b: common_assets._imports_0$1
-  };
+if (!Array) {
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  _easycom_uni_icons2();
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-2dad6c07"]]);
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+if (!Math) {
+  _easycom_uni_icons();
+}
+const _sfc_main = {
+  __name: "preview",
+  setup(__props) {
+    let showFlag = common_vendor.ref(true);
+    const maskShow = () => {
+      console.log("asd");
+      showFlag.value = !showFlag.value;
+    };
+    return (_ctx, _cache) => {
+      return {
+        a: common_vendor.f(5, (item, k0, i0) => {
+          return {};
+        }),
+        b: common_assets._imports_0$1,
+        c: common_vendor.o(maskShow),
+        d: common_vendor.p({
+          type: "left",
+          size: "30"
+        }),
+        e: common_vendor.p({
+          type: "info",
+          size: "25"
+        }),
+        f: common_vendor.p({
+          type: "star",
+          size: "25"
+        }),
+        g: common_vendor.p({
+          type: "download",
+          size: "25"
+        }),
+        h: common_vendor.unref(showFlag)
+      };
+    };
+  }
+};
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__scopeId", "data-v-2dad6c07"]]);
 wx.createPage(MiniProgramPage);
